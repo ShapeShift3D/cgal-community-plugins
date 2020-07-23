d8f8b219955eca4db578c5316b34324197b6c52a
Patrick Laurin <patrick.laurin@shapeshift3d.com>
2020-07-23 13:58:55 -0400

    Revert "compatibility with 4.13.1"
    This reverts commit b696515673cfae29434606370a5433debe8dbf7d.

--- Plugins/CGAL/vtkCGAL/vtkCGALARAPUVParametrization.cxx
+++ Plugins/CGAL/vtkCGAL/vtkCGALARAPUVParametrization.cxx
@@ -98,14 +98,13 @@ int vtkCGALARAPUVParametrization::RequestData(vtkInformation*,
 	UV_pmap uv_map = surfaceMesh.add_property_map<vertex_descriptor, Point_2>("h:uv").first;
 	
 	typedef SMP::Two_vertices_parameterizer_3<Surface_Mesh> Border_Parameterizer;
-	typedef SMP::ARAP_parameterizer_3<Surface_Mesh, Border_Parameterizer> Parameterizer;
 
 	SMP::Error_code err = SMP::parameterize(surfaceMesh,
-		Parameterizer(
-		Border_Parameterizer(),
-			CGAL::Eigen_solver_traits<Eigen::BiCGSTAB<CGAL::Eigen_sparse_matrix<double>::EigenType>>(),
+		SMP::ARAP_parameterizer_3<Surface_Mesh, Border_Parameterizer>(
+			Border_Parameterizer(),
+			CGAL::Eigen_solver_traits<Eigen::SparseLU<CGAL::Eigen_sparse_matrix<double>::EigenType>>(),
 			this->Lambda,
-			(unsigned int)this->MaximumNumberOfIterations,
+			this->MaximumNumberOfIterations,
 			this->Tolerance),
 		bhd,
 		uv_map);
