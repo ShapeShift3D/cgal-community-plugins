c3e1b19371d9e11d737de7472c0b3e6cf820300e
Chandan Satija <chandan.satija@shapeshift3d.com>
2021-12-22 16:44:42 +0000

    Cleaned redundant includes

--- src/stkCGAL3DPolyhedralMesher.cxx
+++ src/stkCGAL3DPolyhedralMesher.cxx
@@ -10,8 +10,6 @@
 #include <vtkStaticPointLocator.h>
 #include <vtkUnstructuredGrid.h>
 
-#include <CGAL/Polygon_mesh_processing/repair_self_intersections.h>
-
 #include <vtkTimerLog.h>
 
 //---------CGAL---------------------------------
